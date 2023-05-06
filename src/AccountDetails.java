07f90ef780a30a6f1b214fe8136316a949deba9e
Dharmateja176 <dharmateja.176@gmail.com>
2023-05-06 10:31:26 +0530

    Updat AccountDetails

--- src/AccountDetails.java
+++ src/AccountDetails.java
@@ -1,50 +1,71 @@
 //Question 7
-import java.util.Arrays;
 import java.util.Scanner;
 
 //Name this class as Account
-class AccountBase{
+//will be
+// class Account {
+class AccountBase {
     protected String accountNumber;
     protected double balance;
     protected String accountHolderName;
+
+    public AccountBase(String accountNumber, double balance, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.accountHolderName = accountHolderName;
+    }
 }
+
 //Change AccountBase name accordingly to that you used in above baseClass
-class SavingAccount extends AccountBase{
+//will be
+//class SavingAccount extends Account {
+class SavingAccount extends AccountBase {
     protected double minimumBalance;
+
+    public SavingAccount(String accountNumber, double balance, String accountHolderName, double minimumBalance) {
+        super(accountNumber, balance, accountHolderName);
+        this.minimumBalance = minimumBalance;
+    }
 }
-class FixedAccount extends SavingAccount{
+
+//Change AccountBase name accordingly to that you used in above baseClass
+//will be
+//class FixedAccount extends Account {
+class FixedAccount extends SavingAccount {
     private int lockingPeriod;
+
+    public FixedAccount(String accountNumber, double balance, String accountHolderName, double minimumBalance, int lockingPeriod) {
+        super(accountNumber, balance, accountHolderName, minimumBalance);
+        this.lockingPeriod = lockingPeriod;
+    }
 
     public int getLockingPeriod() {
         return lockingPeriod;
     }
-
-    public void setLockingPeriod(int lockingPeriod) {
-        this.lockingPeriod = lockingPeriod;
-    }
 }
 
 class AccountBO{
-    public void getAccountDetail(String detail){
+    public  AccountBO(String detail){
         String[] array = detail.split(",");
-        System.out.println(Arrays.toString(array));
         System.out.format("%-20s %-10s %-20s %-20s %s\n","Account Number","Balance","Account holder name","Minimum balance","Locking period");
-        FixedAccount fa = new FixedAccount();
-        fa.accountNumber = array[0];
-        fa.balance = Double.parseDouble(array[1]);
-        fa.accountHolderName =array[2];
-        fa.minimumBalance = Integer.parseInt(array[3]);
-        fa.setLockingPeriod(Integer.parseInt(array[4]));
-        System.out.format("%-20s %-10.2f %-20s %-20.2f %d\n",fa.accountNumber,fa.balance,fa.accountHolderName,fa.minimumBalance,fa.getLockingPeriod());
-
+        FixedAccount fa = new FixedAccount(
+                array[0],
+                Double.parseDouble(array[1]),
+                array[2],
+                Integer.parseInt(array[3]),
+                Integer.parseInt(array[4])
+        );
+        System.out.format("%-20s %-10.2f %-20s %-20.2f %d\n",
+                fa.accountNumber,fa.balance,fa.accountHolderName,fa.minimumBalance,fa.getLockingPeriod());
     }
 }
 //Name this class as Main
+//will be
+//public class Main
 public class AccountDetails {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String detail = sc.nextLine();
-        AccountBO bo = new AccountBO();
-        bo.getAccountDetail(detail);
+        AccountBO bo = new AccountBO(detail);
     }
 }
